c87f9a2708ecee49f058fda6bc6dcc17dc78ba57
RedtakiNi <redtakini@gmail.com>
2023-09-13 15:59:53 +0300

    Add Line 7,8

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,6 +1,8 @@
 n1 = 1
 n2 = n1 + 6
-n3 = int(((n3 * 3) + 14) / 5)
+n3 = int(((n2 * 3) + 14) / 5)
 n4 = int((n3 / 7) - 1)
 n5 = n4 + 1
 n6 = n5 * 6 - 3
+s = str(n1) + str(n2) + str(n3) + str(n4) + str(n5) + str(n6)
+print(s)
